960795d3d79296fc0d3ade349b2f056adbd1cb9d
Raymon Hardy <raymonhardy@icloud.com>
2026-02-17 11:12:44 -0700

    new things

--- CMD.py
+++ CMD.py
@@ -6,6 +6,8 @@ from twilio.rest import Client
 import sys
 print(sys.path)
 
+password = Password1234!
+
 # Reads in Text File that contains the text message/ returns the text in a string
 def readTextFile(text_file):
     text_file = open(text_file, "r")
